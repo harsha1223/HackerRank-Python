9755c134ad5bf9bda092ef371d2fc8642035b6db
Harsha Varshney <66527713+harsha1223@users.noreply.github.com>
2021-04-16 23:02:16 +0530

    Add files via upload

---
+++ CheckStrictSuperset .py
@@ -0,0 +1,12 @@
+A = set(map(int, input().split()))
+N = int(input())
+set_list = []
+
+for i in range(N):
+    x = set(map(int,input().split()))
+    set_list.append(x)
+a = 'True'
+for j in set_list:
+    if not A.issuperset(j):
+        a = 'False'
+print(a)
