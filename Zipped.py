769729b25054e386a995241ac00c0c98a47f9caa
Harsha Varshney <66527713+harsha1223@users.noreply.github.com>
2021-04-16 23:24:13 +0530

    Add files via upload

---
+++ Zipped.py
@@ -0,0 +1,10 @@
+N , X = map(int,input().split())
+MarksSheet = []
+for i in range (X):
+    MarksSheet.append(map(float,input().split()))
+    
+for i in  (zip(*MarksSheet)):
+    sum_of_no = sum(i)
+    total_no_of_subject = len(i)
+    average = sum_of_no / total_no_of_subject
+    print(average)
